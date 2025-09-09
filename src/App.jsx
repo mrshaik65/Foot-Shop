b62056a8eb989b2a544de219800d2773da42cc0e
mrshaik65 <shaikmusthafa110223@gmail.com>
2025-09-09 14:56:50 +0530

    React Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home"
-import Products from "./Components/Products/products";
-import Cart from "./Components/Cart/cart";
+import Products from "./Components/Products/Products";
+import Cart from "./Components/Cart/Cart";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
